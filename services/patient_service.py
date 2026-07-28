0f3a0efedaa33eab1e6819f99d718835fe20db00
Anirudh Chunara <aniruddhchunara1612@gmail.com>
2026-07-28 18:33:10 +0530

    Add patient creation service

--- services/patient_service.py
+++ services/patient_service.py
@@ -1,8 +1,14 @@
 from repository import get_all_patients
-
+from database.database import add_patient
 
 def fetch_all_patients():
     """
     Fetch all patients from the repository.
     """
     return get_all_patients()
+
+def create_patient(patient):
+    """
+    Save a patient using the database layer.
+    """
+    add_patient(patient)
