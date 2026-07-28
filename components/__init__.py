73aca39cb806e6b2d776aaa93cb68bb59c1d85c6
Anirudh Chunara <aniruddhchunara1612@gmail.com>
2026-07-28 15:51:31 +0530

    Create component package interface

---
+++ components/__init__.py
@@ -0,0 +1,22 @@
+from .theme import load_theme
+
+from .cards import kpi_card
+
+from .charts import (
+    bmi_distribution_chart,
+    age_distribution_chart,
+    weight_chart,
+    height_chart
+)
+
+from .tables import (
+    data_table,
+    recent_patients,
+    top_bmi_table
+)
+
+from .sidebar import app_sidebar
+
+from .navbar import navbar
+
+from .footer import footer
