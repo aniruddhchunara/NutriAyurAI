00e06a1f6bd63d95892d7c918bd0225baafb1a8e
Anirudh Chunara <aniruddhchunara1612@gmail.com>
2026-08-26 21:01:08 +0530

    fix: initialize database on streamlit startup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+from database.database import (
+    create_table,
+    create_appointment_table,
+    create_diet_plan_tables
+)
+
 
 st.set_page_config(
     page_title="NutriAyurAI",
@@ -8,6 +14,11 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+
+create_table()
+create_appointment_table()
+create_diet_plan_tables()
 
 
 
